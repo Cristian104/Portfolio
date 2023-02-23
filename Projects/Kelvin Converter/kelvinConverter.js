d15153be782af6b4b0e37da9838ba96a655f3a6a
Cristian <51556485+Cristian104@users.noreply.github.com>
2023-02-23 21:06:27 +0000

    Structured documentation for the project

--- Projects/Kelvin Converter/kelvinConverter.js
+++ Projects/Kelvin Converter/kelvinConverter.js
@@ -1,8 +1,14 @@
 // First we declare the Kelvin variable
 const kelvin = 293;
+console.log(
+  "The temperature is",
+  kelvin,
+  "degrees Kelvin and we're converting it to Celsius and Fahrenheit\n"
+);
 
 // We set the value for celsius and add the formula
 const celsius = kelvin - 273;
+console.log("The temperature is", celsius, "degrees Celsius");
 
 // we convert the kelvin to Fahrenheit
 const fahrenheit = celsius * (9 / 5) + 32;
---
+++ Projects/Kelvin Converter/log.md
@@ -0,0 +1,3 @@
+# This is a Kelvin converter for any other conversion that we have
+
+This converter allows you to get the prompt for Celsius and Fahrenheit temperature.
